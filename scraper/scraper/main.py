a2f05e31a003681b290d2634b6ed612ad13cdf07
Steven Hansel <stevenhanselgo@gmail.com>
2022-10-09 18:11:44 +0700

    feat: create initial login api

--- scraper/scraper/main.py
+++ scraper/scraper/main.py
@@ -1,13 +1,27 @@
+from scraper.binus import BinusScraper
+
 import uvicorn
-from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi import Depends, FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+class LoginRequestBody(BaseModel):
+    email: str
+    password: str
+
+@app.post("/v1/login")
+async def login(
+        body: LoginRequestBody,
+        binus: BinusScraper = Depends(BinusScraper),
+    ):
+    return await binus.login(body.email, body.password)
+
 def start():
     """Launched with `poetry run start` at root level"""
-    uvicorn.run("scraper.main:app", host="0.0.0.0", port=8000, reload=True)
+    # todo: find free port
+    uvicorn.run("scraper.main:app", host="0.0.0.0", port=8080, reload=True)
