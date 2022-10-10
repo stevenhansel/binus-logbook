3a9deeaa6c6c8c9c2342e1c2b4e9b339000d2f2a
Steven Hansel <stevenhanselgo@gmail.com>
2022-10-10 15:23:39 +0700

    feat: create basic controller setup

--- scraper2/cmd/api/main.go
+++ scraper2/cmd/api/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/playwright-community/playwright-go"
 	"go.uber.org/zap"
 
 	"github.com/stevenhansel/binus-logbook/scraper/internal/container"
 	internalhttp "github.com/stevenhansel/binus-logbook/scraper/internal/http"
+	"github.com/stevenhansel/binus-logbook/scraper/internal/http/responseutil"
 )
 
 func main() {
@@ -22,7 +25,13 @@ func main() {
 		os.Exit(1)
 	}
 
-	container := container.New(log)
+	if err := playwright.Install(); err != nil {
+		log.Error("An error occurred during the installation process of playwright", zap.String("error", fmt.Sprint(err)))
+	}
+
+	responseutil := responseutil.New(log)
+
+	container := container.New(log, responseutil)
 	server := internalhttp.New(container)
 
 	stop := make(chan struct{})
@@ -32,7 +41,7 @@ func main() {
 		<-sigint
 
 		if err := server.Shutdown(ctx); err != nil {
-			log.Error("An error occurred during shutting down the server")
+			log.Error("An error occurred during shutting down the server", zap.String("error", fmt.Sprint(err)))
 		}
 
 		close(stop)
@@ -40,7 +49,7 @@ func main() {
 
 	log.Info("Scraper API has started running on http://localhost:8080")
 	if err := server.Serve(); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		log.Error("An error occurred during serving the server")
+		log.Error("An error occurred during serving the server", zap.String("error", fmt.Sprint(err)))
 	}
 
 	<-stop
---
+++ scraper2/internal/http/responseutil/errors.go
@@ -0,0 +1,7 @@
+package responseutil
+
+type ErrorCode string
+
+const (
+	InternalServerError ErrorCode = "INTERNAL_SERVER_ERROR"
+)
