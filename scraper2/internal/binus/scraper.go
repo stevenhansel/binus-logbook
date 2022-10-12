91af03a4e4399586ed6163e3a6e0c3c7d3374bdf
Steven Hansel <stevenhanselgo@gmail.com>
2022-10-12 22:56:04 +0700

    feat: finish initial implementation of login scraper

--- scraper2/internal/binus/scraper.go
+++ scraper2/internal/binus/scraper.go
@@ -1,13 +1,15 @@
 package binus
 
 import (
+	"reflect"
 	"strings"
 
 	"github.com/playwright-community/playwright-go"
 )
 
 const (
-	LoginPageURL = "https://enrichment.apps.binus.ac.id/Login/Student/Login"
+	LoginPageURL      = "https://enrichment.apps.binus.ac.id/Login/Student/Login"
+	ChangeSemesterURL = "https://enrichment.apps.binus.ac.id/Dashboard/Student/IndexStudentDashboard"
 )
 
 type BinusScraper struct{}
@@ -17,10 +19,10 @@ func New() *BinusScraper {
 }
 
 type Student struct {
-	Name              string
-	Email             string
-	ProfilePictureURL string
-	Enrichments       []Enrichment
+	Name  string
+	Email string
+	// ProfilePictureURL string (do this later because base64 encoding)
+	Enrichments []Enrichment
 }
 
 type Enrichment struct {
@@ -38,11 +40,31 @@ type Enrichment struct {
 	OfficePhoneNumber         string
 }
 
+type Semester struct {
+	Name  string
+	Value string
+}
+
+var studentDashboardParseTable = map[string]string{
+	"ENRICHMENT_TRACK":              "Track",
+	"Company Partner:":              "CompanyPartner",
+	"Position":                      "Position",
+	"Location:":                     "Location",
+	"Working/Office Hours:":         "WorkingOfficeHours",
+	"Site Supervisor:":              "SiteSupervisorName",
+	"Site Supervisor Email:":        "SiteSupervisorEmail",
+	"Site Supervisor Phone Number:": "SiteSupervisorPhoneNumber",
+	"Office Phone Number:":          "OfficePhoneNumber",
+	"Faculty Supervisor:":           "FacultySupervisor",
+}
+
 func (s *BinusScraper) Login(email, password string) (*Student, error) {
 	pw, err := playwright.Run()
 	if err != nil {
 		return nil, err
 	}
+
+	defer pw.Stop()
 
 	headless := false
 	browser, err := pw.Chromium.Launch(playwright.BrowserTypeLaunchOptions{
@@ -51,6 +73,8 @@ func (s *BinusScraper) Login(email, password string) (*Student, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	defer browser.Close()
 
 	page, err := browser.NewPage()
 	if err != nil {
@@ -86,7 +110,7 @@ func (s *BinusScraper) Login(email, password string) (*Student, error) {
 		return nil, err
 	}
 
-	page.WaitForLoadState("networkidle")
+	page.WaitForSelector("#StudentTermDashboard")
 
 	studentNameNode, err := page.QuerySelector("h2[class=student-name]")
 	if err != nil {
@@ -98,28 +122,89 @@ func (s *BinusScraper) Login(email, password string) (*Student, error) {
 	}
 	studentName = strings.TrimSpace(strings.Split(studentName, "\n")[0])
 
-	page.WaitForTimeout(10000)
-
-	// entries, err := page.QuerySelectorAll(".athing")
-	// if err != nil {
-	// }
-
-	// for i, entry := range entries {
-	// 	titleElement, err := entry.QuerySelector("td.title > a")
-	// 	if err != nil {
-	// 	}
-	// 	title, err := titleElement.TextContent()
-	// 	if err != nil {
-	// 	}
-	// 	fmt.Printf("%d: %s\n", i+1, title)
-	// }
-
-	if err = browser.Close(); err != nil {
-		return nil, err
-	}
-	if err = pw.Stop(); err != nil {
+	semesterSelectInput, err := page.QuerySelector("select[name=ddSemester]")
+	if err != nil {
 		return nil, err
 	}
 
-	return &Student{}, nil
+	selectOptionNodes, err := semesterSelectInput.QuerySelectorAll("xpath=child::*")
+	if err != nil {
+		return nil, err
+	}
+
+	semesters := []*Semester{}
+	for _, option := range selectOptionNodes {
+		semesterName, err := option.InnerText()
+		if err != nil {
+			return nil, err
+		}
+		semesterValue, err := option.GetAttribute("value")
+		if err != nil {
+			return nil, err
+		}
+
+		semesters = append(semesters, &Semester{
+			Name:  strings.TrimSpace(semesterName),
+			Value: semesterValue,
+		})
+	}
+
+	enrichments := []Enrichment{}
+	for _, semester := range semesters {
+		changeSemesterSelectInput, err := page.QuerySelector("select[name=ddSemester]")
+		if err != nil {
+			return nil, err
+		}
+
+		optionValues := []string{semester.Value}
+		changeSemesterSelectInput.SelectOption(playwright.SelectOptionValues{Values: &optionValues})
+
+		page.WaitForResponse(ChangeSemesterURL)
+		page.WaitForTimeout(2000) // Add a slight delay just in case
+
+		dataRows := map[string]string{}
+		nodes, err := page.QuerySelectorAll(".column.info")
+		if err != nil {
+			return nil, err
+		}
+
+		for _, node := range nodes {
+			text, err := node.InnerText()
+			if err != nil {
+				return nil, err
+			}
+
+			data := strings.Split(text, "\n")
+			if len(data) < 2 {
+				continue
+			}
+
+			key := data[0]
+			value := data[1]
+			if structFieldName, ok := studentDashboardParseTable[key]; ok {
+				dataRows[structFieldName] = value
+			}
+		}
+
+		currentEnrichment := Enrichment{}
+
+		val := reflect.ValueOf(&currentEnrichment).Elem()
+		for i := 0; i < val.NumField(); i++ {
+			fieldName := val.Type().Field(i).Name
+			if rowValue, ok := dataRows[fieldName]; ok {
+				val.Field(i).SetString(rowValue)
+			}
+		}
+
+		currentEnrichment.SemesterName = semester.Name
+		currentEnrichment.SemesterValue = semester.Value
+
+		enrichments = append(enrichments, currentEnrichment)
+	}
+
+	return &Student{
+		Name:        studentName,
+		Email:       email,
+		Enrichments: enrichments,
+	}, nil
 }
