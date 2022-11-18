73920939a455ba8e953adc6a2436f4db7b220b8c
Jovan Frandika <jovan.frandika@makmur.id>
2022-11-18 23:40:24 +0700

    feat: Migrate to Astro

--- src/types/hooks.ts
+++
@@ -1,9 +0,0 @@
-export interface HookState<R = any> {
-  data?: R
-  isLoading: boolean
-  error: string
-  isFired?: boolean
-  refetch?: () => void
-}
-
-export type UseMutation<R = any> = () => [(args: any) => Promise<void>, HookState<R>]
